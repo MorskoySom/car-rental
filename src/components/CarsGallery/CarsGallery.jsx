4d30ee8924f5919a54b7b995106295145be23f26
MorskoySom <Somovvitvit@gmail.com>
2024-02-24 19:03:34 +0200

    pre_add_loadmorebtn

--- src/components/CarsGallery/CarsGallery.jsx
+++ src/components/CarsGallery/CarsGallery.jsx
@@ -10,27 +10,13 @@ export const CarGallery = () => {
 
     useEffect(() => {
         const fetchDataAsync = async () => {
-            const data = await fetchData();
-            setDataCars(data);
-        };
+            const data = await fetchData()
+            setDataCars(data)
+        }
 
-        fetchDataAsync();
-    }, []);
+        fetchDataAsync()
+    }, [])
 
-
-
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         try {
-    //             const response = await axios.get('https://65d7c1b127d9a3bc1d7bc0a4.mockapi.io/adverts');
-    //             setDataCars(response.data);
-    //         } catch (error) {
-    //             console.error('Error fetching data:', error);
-    //         }
-    //     }
-
-    //     fetchData();
-    // }, []);
 
     return (
         <>
--- src/components/api12.js
+++ src/components/api12.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 
 const BASE_URL = 'https://65d7c1b127d9a3bc1d7bc0a4.mockapi.io/adverts'
 
-export const fetchData = async () => {
+export const fetchData = async (page) => {
+    const params = new URLSearchParams({        
+        per_page: 12,
+        page: page
+    });
     try {
-        const response = await axios.get(`${BASE_URL}`);
+        const response = await axios.get(`${BASE_URL}?${params}`);
         return response.data;
     } catch (error) {
         console.error('Error fetching data:', error);
