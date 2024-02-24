9f55ae8b06c479182b2e38a504f4a8fa75bb41a4
MorskoySom <Somovvitvit@gmail.com>
2024-02-25 01:28:53 +0200

    noredax

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
- import { Provider } from "react-redux";
-import { store } from 'components/redux/store';
+// import { Provider } from "react-redux";
+// import { store } from 'components/redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(  
   <React.StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
       <BrowserRouter basename='/car-rental'>
         <App />
       </BrowserRouter>
-    </Provider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
