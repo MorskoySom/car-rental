af2d7bcd9d925e1b9ba8baa94935f383004880f9
MorskoySom <Somovvitvit@gmail.com>
2024-02-24 15:44:11 +0200

    add_fav_page

--- src/pages/FavCarsPage.jsx
+++ src/pages/FavCarsPage.jsx
@@ -1,12 +1,27 @@
 import { Filter } from "components/Filter/Filter"
+import React, { useState, useEffect } from 'react'
+import { GalleryView } from '../components/CarsGallery/CarsGallery.styled'
+import { OneGalleryItem } from '../components/OneGalleryItem/OneGalleryItem'
+import datacars from '../components/CarsGallery/cars.json'
 
 export const FavCars = () => {
+    const [favoriteCars, setFavoriteCars] = useState([])
+    
+    useEffect(() => {
+        const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || []
+        setFavoriteCars(storedFavorites);
+    }, [favoriteCars])
+
     return (
         <>
-            <Filter />
-            <div>
-                Favourites cars
-            </div>
+            <Filter />            
+            <GalleryView>                   
+                    {datacars
+                        .filter(car => favoriteCars.includes(car.id))
+                        .map((car) => (
+                            <OneGalleryItem data={car} key={car.id} />
+                        ))}
+            </GalleryView>            
         </>
-    )
-}
+    );
+};
